2588def0607b9dac1fb5b7ee8be2a3d6edda6f8c
Mattis Czternasty Hembre <mattisch@stud.ntnu.no>
2020-08-27 15:59:38 +0200

    KARAN2

--- ITD/Thonny/karan.py
+++ ITD/Thonny/karan.py
@@ -1,6 +1,19 @@
 print("hei")
+spm = ["hvor gammel er mattis","hvor gammel er karan"]
+fasit = [18,19]
+i=0
+poeng = 0
+def nyttSpm():
+    print(spm[i])
+    svar = int(input("Svar: "))
+    if fasit[i]==svar:
+        poeng+= 1
+        print("riktig svar")
+    else:
+        print("feil svar")
 
 
+nyttSpm()
 
 
 
