98168bb4fefc6f387000fcc5ec84d6045ff384fd
Mattis Czternasty Hembre <mattisczthem@gmail.com>
2020-10-07 14:59:10 +0200

    ofl.6itgk

--- ITGK/ovinger/ofl-6/oppgave 1.py
+++ ITGK/ovinger/ofl-6/oppgave 1.py
@@ -1,18 +1,18 @@
-handleliste = ['Melk','Bananer','Brus', 'Rundstykker']
-print(handleliste)
+handleliste = ['Melk','Bananer','Brus', 'Rundstykker',]
+# print(handleliste)
 
 ind = (handleliste.index('Rundstykker'))
 handleliste[ind] = 'Brød'
 
-handleliste.append('a')
-handleliste.append('a')
-handleliste.append('a')
-handleliste.append('a')
-handleliste.append('a')
-print(handleliste)
+# print(handleliste)
 
 def firstHalf(liste):
     lengde = len(liste)//2
     return liste[:lengde]
 
-print(firstHalf(handleliste))
+# print(firstHalf(handleliste))
+
+def exceptFor_last_and_first(liste):
+    return liste[1:-1]
+
+# print(exceptFor_last_and_first(handleliste))
