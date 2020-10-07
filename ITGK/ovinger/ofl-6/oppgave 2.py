95fe1a624113fdd01c7a2d08ad74289d16e8a721
Mattis Czternasty Hembre <mattisczthem@gmail.com>
2020-10-07 15:33:17 +0200

    ofl.6itgk

--- ITGK/ovinger/ofl-6/oppgave 2.py
+++ ITGK/ovinger/ofl-6/oppgave 2.py
@@ -1,4 +1,18 @@
 from random import randint
 
-tall = [randint(1,6) for i in range(5)]
-print(tall)
+def dice():
+    tall = [randint(1,6) for i in range(5)]
+    return tall
+#print(dice())
+
+def antall(n,liste):
+    x = 0
+    for i in liste:
+        if i == n:
+            x+=1
+    return x
+    #return liste.count(n) #Mye raksere
+
+terning = dice()
+print(terning)
+print(antall(6, terning))
