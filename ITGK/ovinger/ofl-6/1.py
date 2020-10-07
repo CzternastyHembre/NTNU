88e4b5e8ae23e03fd9107a51d709519d3a149443
Mattis Czternasty Hembre <mattisczthem@gmail.com>
2020-10-07 14:41:21 +0200

    ofl.6itgk

--- ITGK/ovinger/ofl-6/1.py
+++ ITGK/ovinger/ofl-6/1.py
@@ -1,6 +1,12 @@
 # lister
 liste = []
-for i in range(20):
-    liste.append(i)
+indeks = []
+x = 10
+for i in range(x):
+    liste.append(' '+str(i))
+    indeks.append(''+str(-x+i))
 
-print(liste[20])
+# print(liste[2:4]) # fra og med, til(men ikke med)
+# print(liste[::-1])
+# print(liste[::2])
+print(liste,'\n'+str(indeks))
