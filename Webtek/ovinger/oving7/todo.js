98665c41a5091db9f16cb35ba0a12fdba66ad981
Mattis Czternasty Hembre <mattisczthem@gmail.com>
2020-10-14 21:43:51 +0200

    webte-o-7-ferdig-uten mye css

--- Webtek/ovinger/oving7/todo.js
+++ Webtek/ovinger/oving7/todo.js
@@ -1,6 +1,8 @@
 //constants
 const inp = document.querySelector("#inp")
-const btn = document.querySelector("#btn")
+const btnAdd = document.querySelector("#btnAdd")
+const btnRemoveAll = document.querySelector("#btnRemoveAll")
+const btnRemoveLast = document.querySelector("#btnRemoveLast")
 const ul = document.querySelector("#ul")
 const boxStatus = document.querySelector("#boxStatus")
 
@@ -8,11 +10,61 @@ const boxStatus = document.querySelector("#boxStatus")
 let checkboxes = []
 let listeElementer = []
 
-btn.onclick = write //kaller funksjonen når den klikkes
+btnAdd.onclick = btnRemoveLast.onclick = btnRemoveAll.onclick = write //kaller funksjonen når den klikkes
 
-function changeStatus() { //funksjon som endrer statusen hvor mange bokser som er sjekket
+//Funksjoner
+function write(event) {
+
+    if (event.target == btnRemoveAll) { //Hvis det er sletteknappen sletter jeg alt i arrayene og sletter det inni ul
+        checkboxes = []
+        listeElementer = []
+    } else if (event.target == btnRemoveLast) { //Hvis det er sletteknappen sletter jeg alt i arrayene og sletter det inni ul
+        checkboxes.pop()
+        listeElementer.pop()
+    } else if (inp.value == "") { //Sjekker om det faktsik er noe i feltet, hvis ikke returner funksjonen
+        return
+    } else {
+        addTask() //Kjører addtask
+    }
+
+    changeStatus() //Kjører endrestatus fordi når man legger til ett element vil totale tasks øke
+
+    ul.innerHTML = ""
+    for (let i = listeElementer.length - 1; i >= 0; i--) { //appender listene i arrayet men starter på slutten og ender på starten for å skrive ut i riktig rekkefølge
+        ul.appendChild(listeElementer[i])
+    }
+}
+
+function addTask() {
+    //Lager forskjellige elementer som tilslutt legges i ul
+    let liste = document.createElement("li")
+    let h2 = document.createElement("h2")
+    let i = document.createElement("i")
+    let chk = document.createElement("input")
+    let totalTime = fetchTime() //Henter tiden til når den lages
+
+    //Legger til egenskaper på elementene
+    chk.type = "checkbox"
+    chk.className = "chk"
+    chk.checked = false
+    checkboxes.push(chk)
+    chk.onclick = changeStatus
+
+    h2.innerHTML = inp.value
+
+    i.innerHTML = 'Dato: ' + totalTime
+
+    //legger elementene i det nye liste-elementet
+    liste.appendChild(chk)
+    liste.appendChild(h2)
+    liste.appendChild(i)
+
+    //Legger listen inn i listeelementer-arrayet
+    listeElementer.push(liste)
+}
+
+function changeStatus() { //funksjon som endrer statusen og hvor mange bokser som er sjekket
     let boxesChecked = 0
-    let total = 0
 
     for (let i = 0; i < checkboxes.length; i++) {
         if (checkboxes[i].checked) {
@@ -25,48 +77,6 @@ function changeStatus() { //funksjon som endrer statusen hvor mange bokser som e
     boxStatus.innerHTML = 'Finnised:' + boxesChecked + '/' + checkboxes.length
 }
 
-function write() {
-    if (inp.value == "") { //Sjekker om det faktsik er noe i feltet, hvis ikke returner funksjonen
-        return
-    }
-    addTask() //Kjører addtask
-    changeStatus() //Kjører endrestatus fordi når man legger til ett element vil totale tasks øke
-
-    for (let i = listeElementer.length - 1; i >= 0; i--) { //appender listene i arrayet men starter på slutten og ender på starten for å skrive ut i riktig rekkefølge
-        ul.appendChild(listeElementer[i])
-    }
-}
-
-function addTask() {
-    //Lager forskjellige elementer som tilslutt legges i ul
-    let liste = document.createElement("li")
-    let p = document.createElement("p")
-    let i = document.createElement("i")
-    let chk = document.createElement("input")
-    let totalTime = fetchTime() //Henter tiden til når den lages
-
-    //Legger til egenskaper på elementene
-    chk.type = "checkbox"
-    chk.className = "chk"
-    chk.checked = false
-    checkboxes.push(chk)
-    chk.onclick = changeStatus
-
-    p.innerHTML = inp.value
-
-    i.innerHTML = 'Dato: ' + totalTime
-
-    //legger elementene i det nye liste-elementet
-    liste.appendChild(p)
-    liste.appendChild(i)
-    liste.appendChild(chk)
-
-    //Legger listen inn i listeelementer-arrayet
-    listeElementer.push(liste)
-    liste.id = listeElementer.indexOf(liste)
-
-}
-
 function fetchTime() { //funksjon som henter tiden
     let d = new Date()
 
@@ -74,7 +84,7 @@ function fetchTime() { //funksjon som henter tiden
     let minutes = String(d.getMinutes())
     let hours = String(d.getHours())
     let day = String(d.getDate())
-    let month = String(d.getMonth() + 1) //note that Jan=0, Dec=11 (annoying I know)
+    let month = String(d.getMonth() + 1) //merk at Jan=0, Dec=11 (idk hvorfor)
     let year = String(d.getFullYear())
 
     let timeArray = [seconds, minutes, hours, day, month, year]
