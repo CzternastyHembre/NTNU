3a7be051029896ba501a179ccd85294120d933d9
Mattis Czternasty Hembre <mattisczthem@gmail.com>
2020-11-17 20:45:39 +0100

    itgk-o-5.2+div

---
+++ ITGK/Pensum++/Filbehandling/lage filer.py
@@ -0,0 +1,20 @@
+def skrivFil(filnavn):
+    with open(filnavn, 'w') as file:
+        print('Skriv teksen inn')
+        text = input('> ')
+        while text != '':
+            file.write(text + '\n')
+            text = input('> ')
+    print('teksten har blir lagret i '+ filnavn)
+
+def skrivFilFraListe(filnavn):
+    with open(filnavn, 'w') as file:
+        print('Skriv teksen inn')
+        liste = []
+        text = '_'
+        while text != '':
+            text = input('> ')
+            liste.append(text+'\n')
+        file.writelines(liste)
+
+skrivFilFraListe('Min andre fil')
